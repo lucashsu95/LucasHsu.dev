6403c777f213d18eabf086dc1fa89be862500751
lucashsu95 <lucashsu95@gmail.com>
2024-09-13 21:45:55 +0800

    Add git branch and Update python Day8

--- docs/.vitepress/config/sidebar/git.js
+++ docs/.vitepress/config/sidebar/git.js
@@ -5,6 +5,7 @@ export function sidebarGit() {
       items: [
         { text: '初次下載git', link: '/git/初次下載git', activeMatch: '/git/初次下載git' },
         { text: 'git commit 提交', link: '/git/git-commit', activeMatch: '/git/git-commit' },
+        { text: 'git branch 分支', link: '/git/git-branch', activeMatch: '/git/git-branch' },
         { text: 'git clone 克隆', link: '/git/git-clone', activeMatch: '/git/git-clone' },
         { text: '電腦上移除git', link: '/git/電腦上移除git', activeMatch: '/git/電腦上移除git' },
       ],
---
+++ docs/git/git-branch.md
@@ -0,0 +1,12 @@
+# Git Branch 分支
+
+## 介紹
+`Git Branch` 是 `Git` 的一個功能，能夠幫助我們在不同的開發階段中，管理不同的代碼版本。例如，我們可以在開發新功能時，使用一個新的`branch`，當功能完成後，我們可以將這個`branch`合并回master分支，或者將這個branch刪除。這樣能夠幫助我們更好地管理代碼，避免在開發過程中引入`bug`。
+
+## 基本命令
+
+```bash
+git branch
+git branch -r // 查看遠端分支
+git branch -a // 查看所有分支
+```
