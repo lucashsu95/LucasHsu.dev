f7327aa5b961110057f92ebdcc64759e7e0723f0
lucashsu95 <lucashsu95@gmail.com>
2024-10-13 22:55:39 +0800

    clear code

--- docs/.vitepress/config/sidebar/index.js
+++
@@ -1,7 +0,0 @@
-export * from './git';
-export * from './pythonNoob';
-export * from './pythonNation';
-export * from './portfolio';
-export * from './php';
-export * from './css';
-export * from './javascript';
--- docs/javascript/index.md
+++
@@ -1,3 +0,0 @@
-# JavaScript
-
-網頁中的邏輯擔當，都是由 JavaScript 組成。
