68d0ac600b4210f215702b7505f262a82bb985ff
lucashsu95 <lucashsu95@users.noreply.github.com>
2024-10-20 18:35:51 +0000

    deploy: fe0eab97d9dcbc5aca246aaec7b8285c3f70d57e

---
+++ assets/python_新手上路系列_tqc_tqc6_607.md.7zS4ZJKe.js
@@ -0,0 +1,23 @@
+import{_ as e,c as t,J as s,V as l,E as a,o as p}from"./chunks/framework.RK-3fdBv.js";const u=JSON.parse('{"title":"TQC+ 程式語言Python 607 成績計算","description":"","frontmatter":{},"headers":[],"relativePath":"python/新手上路系列/tqc/tqc6/607.md","filePath":"python/新手上路系列/tqc/tqc6/607.md","lastUpdated":1727859319000}'),h={name:"python/新手上路系列/tqc/tqc6/607.md"},k=l(`<h1 id="tqc-程式語言python-607-成績計算" tabindex="-1">TQC+ 程式語言Python 607 成績計算 <a class="header-anchor" href="#tqc-程式語言python-607-成績計算" aria-label="Permalink to &quot;TQC+ 程式語言Python 607 成績計算&quot;">​</a></h1><p>題目說明：請撰寫一程式，讓使用者輸入三位學生各五筆成績，接著再計算並輸出每位學生的總分及平均分數。</p><blockquote><p>提示：平均分數輸出到小數點後第二位。</p></blockquote><h3 id="範例輸入" tabindex="-1">範例輸入 <a class="header-anchor" href="#範例輸入" aria-label="Permalink to &quot;範例輸入&quot;">​</a></h3><div class="language-shell vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">shell</span><pre class="shiki shiki-themes vitesse-light vitesse-dark vp-code"><code><span class="line"><span style="--shiki-light:#59873A;--shiki-dark:#80A665;">78</span></span>
+<span class="line"><span style="--shiki-light:#59873A;--shiki-dark:#80A665;">89</span></span>
+<span class="line"><span style="--shiki-light:#59873A;--shiki-dark:#80A665;">88</span></span>
+<span class="line"><span style="--shiki-light:#59873A;--shiki-dark:#80A665;">70</span></span>
+<span class="line"><span style="--shiki-light:#59873A;--shiki-dark:#80A665;">60</span></span>
+<span class="line"><span style="--shiki-light:#59873A;--shiki-dark:#80A665;">90</span></span>
+<span class="line"><span style="--shiki-light:#59873A;--shiki-dark:#80A665;">78</span></span>
+<span class="line"><span style="--shiki-light:#59873A;--shiki-dark:#80A665;">66</span></span>
+<span class="line"><span style="--shiki-light:#59873A;--shiki-dark:#80A665;">68</span></span>
+<span class="line"><span style="--shiki-light:#59873A;--shiki-dark:#80A665;">78</span></span>
+<span class="line"><span style="--shiki-light:#59873A;--shiki-dark:#80A665;">69</span></span>
+<span class="line"><span style="--shiki-light:#59873A;--shiki-dark:#80A665;">97</span></span>
+<span class="line"><span style="--shiki-light:#59873A;--shiki-dark:#80A665;">70</span></span>
+<span class="line"><span style="--shiki-light:#59873A;--shiki-dark:#80A665;">89</span></span>
+<span class="line"><span style="--shiki-light:#59873A;--shiki-dark:#80A665;">90</span></span></code></pre></div><h3 id="範例輸出" tabindex="-1">範例輸出 <a class="header-anchor" href="#範例輸出" aria-label="Permalink to &quot;範例輸出&quot;">​</a></h3><div class="language-shell vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">shell</span><pre class="shiki shiki-themes vitesse-light vitesse-dark vp-code"><code><span class="line"><span style="--shiki-light:#59873A;--shiki-dark:#80A665;">Student</span><span style="--shiki-light:#2F798A;--shiki-dark:#4C9A91;"> 1</span></span>
+<span class="line"><span style="--shiki-light:#A0ADA0;--shiki-dark:#758575DD;">#Sum 385</span></span>
+<span class="line"><span style="--shiki-light:#A0ADA0;--shiki-dark:#758575DD;">#Average 77.00</span></span>
+<span class="line"><span style="--shiki-light:#59873A;--shiki-dark:#80A665;">Student</span><span style="--shiki-light:#2F798A;--shiki-dark:#4C9A91;"> 2</span></span>
+<span class="line"><span style="--shiki-light:#A0ADA0;--shiki-dark:#758575DD;">#Sum 380</span></span>
+<span class="line"><span style="--shiki-light:#A0ADA0;--shiki-dark:#758575DD;">#Average 76.00</span></span>
+<span class="line"><span style="--shiki-light:#59873A;--shiki-dark:#80A665;">Student</span><span style="--shiki-light:#2F798A;--shiki-dark:#4C9A91;"> 3</span></span>
+<span class="line"><span style="--shiki-light:#A0ADA0;--shiki-dark:#758575DD;">#Sum 415</span></span>
+<span class="line"><span style="--shiki-light:#A0ADA0;--shiki-dark:#758575DD;">#Average 83.00</span></span></code></pre></div>`,7);function o(c,r,d,A,g,y){const i=a("NolebaseGitContributors"),n=a("NolebaseGitChangelog");return p(),t("div",null,[k,s(i),s(n)])}const m=e(h,[["render",o]]);export{u as __pageData,m as default};
