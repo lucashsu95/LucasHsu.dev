713ad5c41ca67eb1dfbdd723f2277b8da2ca1c3f
lucashsu95 <lucashsu95@users.noreply.github.com>
2025-01-11 11:56:06 +0000

    deploy: 397f80a318a28cfb8bf5abf7db26a05f1e922753

---
+++ assets/python_新手上路系列_loop-ten-question.md.KWwxRcVm.js
@@ -0,0 +1,81 @@
+import{_ as e,c as l,J as s,V as i,E as a,o as t}from"./chunks/framework.RK-3fdBv.js";const k=JSON.parse('{"title":"迴圈10題","description":"","frontmatter":{},"headers":[],"relativePath":"python/新手上路系列/loop-ten-question.md","filePath":"python/新手上路系列/loop-ten-question.md","lastUpdated":1736596539000}'),c={name:"python/新手上路系列/loop-ten-question.md"},o=i(`<h1 id="迴圈10題" tabindex="-1">迴圈10題 <a class="header-anchor" href="#迴圈10題" aria-label="Permalink to &quot;迴圈10題&quot;">​</a></h1><p>請撰寫程式，輸出以下圖形。</p><h2 id="_1" tabindex="-1">(1) <a class="header-anchor" href="#_1" aria-label="Permalink to &quot;(1)&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes vitesse-light vitesse-dark vp-code"><code><span class="line"><span>＊．．．．．．．．</span></span>
+<span class="line"><span>．＊．．．．．．．</span></span>
+<span class="line"><span>．．＊．．．．．．</span></span>
+<span class="line"><span>．．．＊．．．．．</span></span>
+<span class="line"><span>．．．．＊．．．．</span></span>
+<span class="line"><span>．．．．．＊．．．</span></span>
+<span class="line"><span>．．．．．．＊．．</span></span>
+<span class="line"><span>．．．．．．．＊．</span></span>
+<span class="line"><span>．．．．．．．．＊</span></span></code></pre></div><h2 id="_2" tabindex="-1">(2) <a class="header-anchor" href="#_2" aria-label="Permalink to &quot;(2)&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes vitesse-light vitesse-dark vp-code"><code><span class="line"><span>＊＊＊＊＊＊＊＊＊</span></span>
+<span class="line"><span>＊．．．．．．．＊</span></span>
+<span class="line"><span>＊．．．．．．．＊</span></span>
+<span class="line"><span>＊．．．．．．．＊</span></span>
+<span class="line"><span>＊．．．．．．．＊</span></span>
+<span class="line"><span>＊．．．．．．．＊</span></span>
+<span class="line"><span>＊．．．．．．．＊</span></span>
+<span class="line"><span>＊．．．．．．．＊</span></span>
+<span class="line"><span>＊＊＊＊＊＊＊＊＊</span></span></code></pre></div><h2 id="_3" tabindex="-1">(3) <a class="header-anchor" href="#_3" aria-label="Permalink to &quot;(3)&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes vitesse-light vitesse-dark vp-code"><code><span class="line"><span>．．．．＊．．．．</span></span>
+<span class="line"><span>．．．．＊．．．．</span></span>
+<span class="line"><span>．．．．＊．．．．</span></span>
+<span class="line"><span>．．．．＊．．．．</span></span>
+<span class="line"><span>＊＊＊＊＊＊＊＊＊</span></span>
+<span class="line"><span>．．．．＊．．．．</span></span>
+<span class="line"><span>．．．．＊．．．．</span></span>
+<span class="line"><span>．．．．＊．．．．</span></span>
+<span class="line"><span>．．．．＊．．．．</span></span></code></pre></div><h2 id="_4" tabindex="-1">(4) <a class="header-anchor" href="#_4" aria-label="Permalink to &quot;(4)&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes vitesse-light vitesse-dark vp-code"><code><span class="line"><span>＊＊＊＊＊＊＊＊＊</span></span>
+<span class="line"><span>＊＊＊＊＊＊＊＊＊</span></span>
+<span class="line"><span>＊＊＊＊＊＊＊＊＊</span></span>
+<span class="line"><span>＊＊＊＊＊＊＊＊＊</span></span>
+<span class="line"><span>＊＊＊＊＊＊＊＊＊</span></span>
+<span class="line"><span>．．．．．．．．．</span></span>
+<span class="line"><span>．．．．．．．．．</span></span>
+<span class="line"><span>．．．．．．．．．</span></span>
+<span class="line"><span>．．．．．．．．．</span></span></code></pre></div><h2 id="_5" tabindex="-1">(5) <a class="header-anchor" href="#_5" aria-label="Permalink to &quot;(5)&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes vitesse-light vitesse-dark vp-code"><code><span class="line"><span>＊＊＊＊＊．．．．</span></span>
+<span class="line"><span>＊＊＊＊＊．．．．</span></span>
+<span class="line"><span>＊＊＊＊＊．．．．</span></span>
+<span class="line"><span>＊＊＊＊＊．．．．</span></span>
+<span class="line"><span>＊＊＊＊＊．．．．</span></span>
+<span class="line"><span>．．．．．．．．．</span></span>
+<span class="line"><span>．．．．．．．．．</span></span>
+<span class="line"><span>．．．．．．．．．</span></span>
+<span class="line"><span>．．．．．．．．．</span></span></code></pre></div><h2 id="_6" tabindex="-1">(6) <a class="header-anchor" href="#_6" aria-label="Permalink to &quot;(6)&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes vitesse-light vitesse-dark vp-code"><code><span class="line"><span>．．．．．．．．＊</span></span>
+<span class="line"><span>．．．．．．．＊．</span></span>
+<span class="line"><span>．．．．．．＊．．</span></span>
+<span class="line"><span>．．．．．＊．．．</span></span>
+<span class="line"><span>．．．．＊．．．．</span></span>
+<span class="line"><span>．．．＊．．．．．</span></span>
+<span class="line"><span>．．＊．．．．．．</span></span>
+<span class="line"><span>．＊．．．．．．．</span></span>
+<span class="line"><span>＊．．．．．．．．</span></span></code></pre></div><h2 id="_7" tabindex="-1">(7) <a class="header-anchor" href="#_7" aria-label="Permalink to &quot;(7)&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes vitesse-light vitesse-dark vp-code"><code><span class="line"><span>＊．＊．＊．＊．＊</span></span>
+<span class="line"><span>．＊．＊．＊．＊．</span></span>
+<span class="line"><span>＊．＊．＊．＊．＊</span></span>
+<span class="line"><span>．＊．＊．＊．＊．</span></span>
+<span class="line"><span>＊．＊．＊．＊．＊</span></span>
+<span class="line"><span>．＊．＊．＊．＊．</span></span>
+<span class="line"><span>＊．＊．＊．＊．＊</span></span>
+<span class="line"><span>．＊．＊．＊．＊．</span></span>
+<span class="line"><span>＊．＊．＊．＊．＊</span></span></code></pre></div><h2 id="_8" tabindex="-1">(8) <a class="header-anchor" href="#_8" aria-label="Permalink to &quot;(8)&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes vitesse-light vitesse-dark vp-code"><code><span class="line"><span>＊．．．＊．．．＊</span></span>
+<span class="line"><span>．．．＊．．．＊．</span></span>
+<span class="line"><span>．．＊．．．＊．．</span></span>
+<span class="line"><span>．＊．．．＊．．．</span></span>
+<span class="line"><span>＊．．．＊．．．＊</span></span>
+<span class="line"><span>．．．＊．．．＊．</span></span>
+<span class="line"><span>．．＊．．．＊．．</span></span>
+<span class="line"><span>．＊．．．＊．．．</span></span>
+<span class="line"><span>＊．．．＊．．．＊</span></span></code></pre></div><h2 id="_9" tabindex="-1">(9) <a class="header-anchor" href="#_9" aria-label="Permalink to &quot;(9)&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes vitesse-light vitesse-dark vp-code"><code><span class="line"><span>．＊＊＊．＊＊＊．</span></span>
+<span class="line"><span>＊＊＊．＊＊＊．＊</span></span>
+<span class="line"><span>＊＊．＊＊＊．＊＊</span></span>
+<span class="line"><span>＊．＊＊＊．＊＊＊</span></span>
+<span class="line"><span>．＊＊＊．＊＊＊．</span></span>
+<span class="line"><span>＊＊＊．＊＊＊．＊</span></span>
+<span class="line"><span>＊＊．＊＊＊．＊＊</span></span>
+<span class="line"><span>＊．＊＊＊．＊＊＊</span></span>
+<span class="line"><span>．＊＊＊．＊＊＊．</span></span></code></pre></div><h2 id="_10" tabindex="-1">(10) <a class="header-anchor" href="#_10" aria-label="Permalink to &quot;(10)&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes vitesse-light vitesse-dark vp-code"><code><span class="line"><span>＊＊＊＊＊＊＊＊＊</span></span>
+<span class="line"><span>．＊＊＊＊＊＊＊＊</span></span>
+<span class="line"><span>．．＊＊＊＊＊＊＊</span></span>
+<span class="line"><span>．．．＊＊＊＊＊＊</span></span>
+<span class="line"><span>．．．．＊＊＊＊＊</span></span>
+<span class="line"><span>．．．．．＊＊＊＊</span></span>
+<span class="line"><span>．．．．．．＊＊＊</span></span>
+<span class="line"><span>．．．．．．．＊＊</span></span>
+<span class="line"><span>．．．．．．．．＊</span></span></code></pre></div>`,22);function d(h,r,v,u,_,b){const n=a("NolebaseGitContributors"),p=a("NolebaseGitChangelog");return t(),l("div",null,[o,s(n),s(p)])}const m=e(c,[["render",d]]);export{k as __pageData,m as default};
